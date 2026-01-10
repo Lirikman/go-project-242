ab5825066a78ccbb3ece9b995cb51c53851f3232
Lirikman <999gothicman999@mail.ru>
2026-01-10 22:46:36 +0700

    added functionality for calculating file size

--- cmd/hexlet-path-size/main.go
+++ cmd/hexlet-path-size/main.go
@@ -5,7 +5,35 @@ import ("fmt"
 	"os"
 	"context"
 	"github.com/urfave/cli/v3"
+	"errors"
 	)
+
+func GetSize(path string) (string, error) {
+	// получаем информацию о пути
+	fileInfo, err := os.Lstat(path)
+		if err != nil {
+			return "", errors.New("Invalid file path")
+			}
+
+	// проверяем, что путь это директория
+	if fileInfo.IsDir() == true {
+		files, err := os.ReadDir(path)
+		if err != nil {
+			return "", fmt.Errorf("Error: %w", err)
+			}
+		var totalSize int64
+		for _, file := range files {
+			fileInfo, _ := file.Info()
+				totalSize += fileInfo.Size()
+				return fmt.Sprintf("%d	%s", totalSize, path), nil
+				}
+	}
+	
+	// иначе, путь это файл
+	size := fileInfo.Size()
+	return fmt.Sprintf("%d	%s", size, path), nil
+}
+
 
 
 func main() {
@@ -21,4 +49,10 @@ func main() {
     if err := cmd.Run(context.Background(), os.Args); err != nil {
         log.Fatal(err)
     }
+    
+    // Проверка расчёта размера файла или папки 
+    var path string
+    fmt.Print("Введите путь к файлу или папке: ")
+    fmt.Scanln(&path)
+    fmt.Println(GetSize(path))
 }
