35ac5b552a39ac60f1af3abbc6defaed6561e83f
Lirikman <999gothicman999@mail.ru>
2026-02-04 22:47:19 +0700

    the  main function has been renamed - GetPathSize, and support for output in MB and GB has been added, tests have been rewritten

--- cmd/hexlet-path-size/main.go
+++ cmd/hexlet-path-size/main.go
@@ -13,6 +13,91 @@ import (
 	"github.com/urfave/cli/v3"
 )
 
+// функция для подсчёта размера файлов в папке
+func GetPathSize(path string, human, all, recursive bool) (string, error) {
+	// получаем информацию о пути
+	fileInfo, err := os.Lstat(path)
+	if err != nil {
+		return "", errors.New("invalid file path")
+	}
+	// проверяем, что путь это директория
+	if fileInfo.IsDir() {
+		var totalSize int64
+		// проверяем что флаг recursive == false
+		if !recursive {
+			// получаем список файлов и папок указанной папки
+			files, err := os.ReadDir(path)
+			if err != nil {
+				return "", fmt.Errorf("error: %w", err)
+			}
+			// получаем размер всех файлов
+			for _, file := range files {
+				fileInfo, _ := file.Info()
+				// проверяем что объект это файл
+				if !fileInfo.IsDir() {
+					// проверяем что флаг all == false и пропускаем скрытые файлы
+					if !all && strings.HasPrefix(fileInfo.Name(), ".") {
+						continue
+						// иначе учитываем размер файла
+					} else {
+						totalSize += fileInfo.Size()
+					}
+				}
+			}
+			return FormatSize(totalSize, human), nil
+			// если флаг recursive == true
+		} else {
+			// получаем размер файлов во всех вложенных папках
+			err := filepath.WalkDir(path, func(path string, d fs.DirEntry, err error) error {
+				if err != nil {
+					return fmt.Errorf("error: %w", err)
+				}
+				// проверяем, что объект это файл
+				if !d.IsDir() {
+					info, _ := d.Info()
+					// проверяем что флаг all == false и пропускаем скрытые файлы
+					if !all && strings.HasPrefix(info.Name(), ".") {
+						totalSize += 0
+					} else {
+						totalSize += info.Size()
+					}
+				}
+				return nil
+			})
+			if err != nil {
+				return "", fmt.Errorf("error: %w", err)
+			}
+			return FormatSize(totalSize, human), nil
+		}
+	}
+	// иначе, путь это файл
+	// проверяем что флаг all == false и что файл скрытый и возвращаем нулевой размер
+	if !all && strings.HasPrefix(fileInfo.Name(), ".") {
+		return "", nil
+	}
+	// если флаг all == true возвращаем размер любого файла
+	return FormatSize(fileInfo.Size(), human), nil
+}
+
+// функция конвертирования размера в человекочитаемый формат
+func FormatSize(size int64, human bool) string {
+	if !human {
+		return fmt.Sprintf("%dB", size)
+	}
+	var msg_size string
+	switch {
+	case size < 1000:
+		msg_size = fmt.Sprintf("%dB", size)
+	case size >= 1000 && size < 1000000:
+		msg_size = fmt.Sprintf("%.1fKB", float64(size)/1000.0)
+	case size >= 1000000 && size < 1000000000:
+		msg_size = fmt.Sprintf("%.1fMB", float64(size)/1000.0/1000.0)
+	default:
+		msg_size = fmt.Sprintf("%.1fGB", float64(size)/1000.0/1000.0/1000.0)
+	}
+	return msg_size
+}
+
 func main() {
 	сmd := &cli.Command{
 		Name:      "hexlet-path-size",
@@ -44,9 +129,8 @@ func main() {
 				return fmt.Errorf("path not entered, operation not possible")
 			}
 			p := cmd.Args().Get(0)
-			size, _ := GetSize(p, cmd.Bool("all"), cmd.Bool("recursive"))
-			fSize := FormatSize(float64(size), cmd.Bool("human"))
-			fmt.Printf("%s\t%s\n", fSize, p)
+			size, _ := GetPathSize(p, cmd.Bool("human"), cmd.Bool("all"), cmd.Bool("recursive"))
+			fmt.Printf("%s\t%s\n", size, p)
 			return nil
 		},
 	}
@@ -54,92 +138,4 @@ func main() {
 	if err := сmd.Run(context.Background(), os.Args); err != nil {
 		log.Fatal(err)
 	}
-}
-
-// функция для подсчёта размера файлов в папке
-func GetSize(path string, all bool, recursive bool) (int64, error) {
-	// получаем информацию о пути
-	fileInfo, err := os.Lstat(path)
-	if err != nil {
-		return 0, errors.New("invalid file path")
-	}
-	// проверяем, что путь это директория
-	if fileInfo.IsDir() {
-		var totalSize int64
-		// проверяем что флаг recursive == false
-		if !recursive {
-			// получаем список файлов и папок указанной папки
-			files, err := os.ReadDir(path)
-			if err != nil {
-				return 0, fmt.Errorf("error: %w", err)
-			}
-			// получаем размер всех файлов
-			for _, file := range files {
-				fileInfo, _ := file.Info()
-				// проверяем что объект это файл
-				if !fileInfo.IsDir() {
-					// проверяем что флаг all == false
-					if !all {
-						// проверяем что файл скрыт и пропускаем его
-						if strings.HasPrefix(fileInfo.Name(), ".") {
-							continue
-							// если не скрыт, то прибавляем к общему размеру
-						} else {
-							totalSize += fileInfo.Size()
-						}
-						// если флаг all == true учитываем все файлы
-					} else {
-						totalSize += fileInfo.Size()
-					}
-				}
-			}
-			return totalSize, nil
-			// если флаг recursive == true
-		} else {
-			// получаем размер файлов во всех вложенных папках
-			err := filepath.Walk(path, func(path string, info fs.FileInfo, err error) error {
-				if err != nil {
-					return fmt.Errorf("error: %w", err)
-				}
-				// проверяем, что объект это файл
-				if !info.IsDir() {
-					// проверяем что флаг all == false
-					if !all {
-						// проверяем что файл не является скрытым
-						if !strings.HasPrefix(info.Name(), ".") {
-							totalSize += info.Size()
-						}
-						// если флаг all == true учитываем все файлы
-					} else {
-						// если флаг all == true учитываем все файлы
-						totalSize += info.Size()
-					}
-				}
-				return nil
-			})
-			if err != nil {
-				return 0, fmt.Errorf("error: %w", err)
-			}
-			return totalSize, nil
-		}
-	}
-	// иначе, путь это файл
-	// проверяем что флаг all == false
-	if !all {
-		// проверяем что файл скрытый и возвращаем нулевой размер
-		if strings.HasPrefix(fileInfo.Name(), ".") {
-			return 0, nil
-		}
-	}
-	// если флаг all == true возвращаем размер любого файла
-	return fileInfo.Size(), nil
-}
-
-// функция конвертирования размера в человекочитаемый формат
-func FormatSize(size float64, human bool) string {
-	if !human {
-		return fmt.Sprintf("%.0fB", size)
-	}
-	sizeMb := size / 1000000.0
-	return fmt.Sprintf("%.1fMB", sizeMb)
 }
